e899736828e60bad9720df3c032634289845d62d
Theodore Sotirelis <tg.sotirelis@gmail.com>
2023-11-24 22:00:54 -0500

    Added definition of Vehicle class in header file.

--- CIS_1202_201_Program_14_Inheritance/Vehicle.h
+++ CIS_1202_201_Program_14_Inheritance/Vehicle.h
@@ -1,5 +1,23 @@
 #ifndef Vehicle_h
 #define Vehicle_h
 
+#include <string>
+
+Class Vehicle
+{
+private:
+    string ManufactName;
+    int yearbuilt;
+    
+public:
+    string getManfactName() const;
+    int getYearBuilt() const;
+    void setManfactName();
+    void setYearBuilt();
+    void displayInfo();
+    
+    Vehicle();
+    ~Vehicle();
+};
 
 #endif
