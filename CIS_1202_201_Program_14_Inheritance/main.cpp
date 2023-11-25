8054bd3cb3a33e1ed88f10b7685ba778a9d5acfc
Theodore Sotirelis <tg.sotirelis@gmail.com>
2023-11-24 23:55:44 -0500

    Added driver in main for testing.

--- CIS_1202_201_Program_14_Inheritance/main.cpp
+++ CIS_1202_201_Program_14_Inheritance/main.cpp
@@ -1,10 +1,65 @@
 #include <iostream>
 #include <string>
 
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
+
 using namespace std;
 
 int main()
 {
+    Vehicle vehicleTemp;
+    Car carTemp;
+    Truck truckTemp;
+    string tempManuFactName;
+    int tempYearBuilt;
+    int tempDoorNumber;
+    double tempTowingCap;
+    
+    
+    cout << "Vehicle: " << endl;
+    cout << "Enter the manufacturer: ";
+    cin >> tempManuFactName;
+    
+    cout << "Enter the year built: ";
+    cin >> tempYearBuilt;
+    
+    vehicleTemp.setYearBuilt(tempYearBuilt);
+    vehicleTemp.setManfactName(tempManuFactName);
+    vehicleTemp.displayVehicleInfo();
+    
+    
+    cout << "Car: " << endl;
+    cout << "Enter the manufacturer: ";
+    cin >> tempManuFactName;
+    
+    cout << "Enter the year built: ";
+    cin >> tempYearBuilt;
+    
+    cout << "Enter the number of doors: ";
+    cin >> tempDoorNumber;
+    
+    carTemp.setYearBuilt(tempYearBuilt);
+    carTemp.setManfactName(tempManuFactName);
+    carTemp.setNumberOfDoors(tempDoorNumber);
+    carTemp.displayVehicleInfo();
+    
+    
+    cout << "Truck: " << endl;
+    cout << "Enter the manufacturer: ";
+    cin >> tempManuFactName;
+    
+    cout << "Enter the year built: ";
+    cin >> tempYearBuilt;
+    
+    cout << "Enter the towing capacity: ";
+    cin >> tempTowingCap;
+    
+    truckTemp.setYearBuilt(tempYearBuilt);
+    truckTemp.setManfactName(tempManuFactName);
+    truckTemp.setTowingCapacity(tempTowingCap);
+    truckTemp.displayVehicleInfo();
     
     return 0;
 }
