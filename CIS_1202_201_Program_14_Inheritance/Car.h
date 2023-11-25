c266ccfd928328629b05a9de73ecb23a7fbb8884
Theodore Sotirelis <tg.sotirelis@gmail.com>
2023-11-24 23:17:22 -0500

    Added definitions of Car and Truck classes in their header files.

--- CIS_1202_201_Program_14_Inheritance/Car.h
+++ CIS_1202_201_Program_14_Inheritance/Car.h
@@ -1,5 +1,20 @@
 #ifndef Car_h
 #define Car_h
 
+#include "Vehicle.h"
+
+class Car:public Vehicle
+{
+private:
+    int numberOfDoors;
+    
+public:
+    int getNumberOfDoors();
+    void setNumberOfDoors();
+    
+    Car();
+    ~Car();
+    
+};
 
 #endif
--- CIS_1202_201_Program_14_Inheritance/Truck.h
+++ CIS_1202_201_Program_14_Inheritance/Truck.h
@@ -1,5 +1,19 @@
 #ifndef Truck_h
 #define Truck_h
 
+#include "Vehicle.h"
+
+class Truck: public Vehicle
+{
+private:
+    double towingCapacity;
+    
+public:
+    double getTowingCapacity() const;
+    void setTowingCapacity(double);
+    
+    Truck();
+    ~Truck();
+};
 
 #endif
